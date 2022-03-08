b5139082f9c457a8dba2414f0eba83de16c384f9
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-08 09:40:05 -0400

    feat: approutes

--- AppRoutes.jsx
+++ AppRoutes.jsx
@@ -4,14 +4,20 @@ import {
   Route
 } from 'react-router-dom'
 import App from './src/App'
-import Login from './src/pages/Login/login'
+import Home from './src/pages/home/home'
+import Login from './src/pages/login/login'
+import MySubjects from './src/pages/mySubjects/mySubjects'
+import NewSubject from './src/pages/newSubject/newSubject'
+import './src/styles/global.scss'
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/minhas-disciplinas" element={<MySubjects />} />
+        <Route path="/cadastrar-disciplina" element={<NewSubject />} />
       </Routes>
     </BrowserRouter>
   )
